50d0e364b802482dbd2706611de17202d35efaeb
Nicolas Chauvet <kwizart@gmail.com>
2009-11-04 23:55:05 +0100

    - Bump rawhide to fc13

--- bump.sh
+++ bump.sh
@@ -9,5 +9,9 @@ for arch in i386 x86_64 ppc ppc64 ; do
     sed -i -e 's/f11/f12/g' fedora-12-$arch-$type.cfg
     sed -i -e 's/fc11/fc12/g' fedora-12-$arch-$type.cfg
     sed -i -e 's/Fedora 11/Fedora 12/g' fedora-12-$arch-$type.cfg
+    sed -i -e 's/Fedora 11/Fedora 12/g' fedora-rawhide-$arch-$type.cfg
+  #Rawhide
+    sed -i -e 's/fc12/fc13/g' fedora-rawhide-$arch-$type.cfg
+    sed -i -e 's/Fedora 12/Fedora 13/g' fedora-rawhide-$arch-$type.cfg
   done
 done
