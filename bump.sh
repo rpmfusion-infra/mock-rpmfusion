b016e27d1230166bec993fa127c5c1aa0950b123
Nicolas Chauvet <kwizart@gmail.com>
2009-11-04 23:42:37 +0100

    Bump script

---
+++ bump.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+for arch in i386 x86_64 ppc ppc64 ; do
+  for type in rpmfusion_free rpmfusion_nonfree kwizart ; do
+    cp -p fedora-11-$arch-$type.cfg fedora-12-$arch-$type.cfg
+    git add fedora-12-$arch-$type.cfg
+    sed -i -e 's/fedora-11/fedora-12/g' fedora-12-$arch-$type.cfg
+    sed -i -e 's/f11/f12/g' fedora-12-$arch-$type.cfg
+    sed -i -e 's/fc11/fc12/g' fedora-12-$arch-$type.cfg
+    sed -i -e 's/Fedora 11/Fedora 12/g' fedora-12-$arch-$type.cfg
+  done
+done
