13aa681638285e133a8b20f3a942cf9e0af2b3d7
Nicolas Chauvet <kwizart@gmail.com>
2012-03-07 23:22:42 +0100

    Introduce armhfp for 15 17 rawhide

--- round.sh
+++ round.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ARCHES="i386"
-FVERSION="13 14 15 rawhide"
+ARCHES="arm"
+FVERSION="15 17 rawhide"
 #FVERSION=rawhide
 REPOS="rpmfusion_free rpmfusion_nonfree kwizart"
 
@@ -19,10 +19,12 @@ for arch in $ARCHES ; do
 
 #sed -i -e "s|@arch@|${arch}|g" fedora-${fver}-${arch}-${repo}.cfg
 #sed -i -e "s|@version@|${fver}|g" fedora-${fver}-${arch}-${repo}.cfg
-for arch2 in arm s390x sparc sparc64 ; do 
+for arch2 in armhfp ; do 
   cp fedora-${fver}-${arch}-${repo}.cfg fedora-${fver}-${arch2}-${repo}.cfg
   git add fedora-${fver}-${arch2}-${repo}.cfg
   sed -i -e "s|${arch}|${arch2}|g" fedora-${fver}-${arch2}-${repo}.cfg
+  sed -i -e "s|armhfpv5tel', 'armhfpv6l', 'armhfpv7l|armv7l|g" fedora-${fver}-${arch2}-${repo}.cfg
+  sed -i -e "s|armhfpv5tel|armhl|g" fedora-${fver}-${arch2}-${repo}.cfg
 done
 
 ### /script
