407030f6be297fdce945360170503bc7f2950acd
Nicolas Chauvet <kwizart@gmail.com>
2012-06-24 14:49:39 +0200

    Round commit

--- round.sh
+++ round.sh
@@ -19,12 +19,12 @@ REPOS="rpmfusion_free rpmfusion_nonfree kwizart"
 
 #sed -i -e "s|@arch@|${arch}|g" fedora-${fver}-${arch}-${repo}.cfg
 #sed -i -e "s|@version@|${fver}|g" fedora-${fver}-${arch}-${repo}.cfg
-for arch2 in ppc ppc64 ; do 
+for arch2 in sparc sparc64 ; do 
   #cp fedora-${fver}-${arch}-${repo}.cfg fedora-${fver}-${arch2}-${repo}.cfg
   #git add fedora-${fver}-${arch2}-${repo}.cfg
   #sed -i -e "s|${arch}|${arch2}|g" fedora-${fver}-${arch2}-${repo}.cfg
   #sed -i -e "s|mirrorlist=http://mirrors.rpmfusion.org|#mirrorlist=http://mirrors.rpmfusion.org|g" fedora-${fver}-${arch2}-${repo}.cfg
-  sed -i -e "s|kojipkgs.fedoraproject.org|ppc.koji.fedoraproject.org|g" fedora-${fver}-${arch2}-${repo}.cfg
+  sed -i -e "s|kojipkgs.fedoraproject.org|sparc.koji.fedoraproject.org|g" fedora-${fver}-${arch2}-${repo}.cfg
   #sed -i -e "s|#baseurl=http://download1.rpmfusion.org/nonfree/fedora/|baseurl=http://download1.rpmfusion.org/nonfree/fedora-secondary/|g" fedora-${fver}-${arch2}-${repo}.cfg
 done
 
