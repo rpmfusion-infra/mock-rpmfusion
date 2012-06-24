d6c6418d00279982aa4926ab0d2ca1a5fffd8660
Nicolas Chauvet <kwizart@gmail.com>
2012-06-24 12:56:30 +0200

    Use fedora-secondary for baseurl

--- round.sh
+++ round.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 ARCHES="arm"
-FVERSION="15 17 rawhide"
+FVERSION="17 rawhide"
 #FVERSION=rawhide
 REPOS="rpmfusion_free rpmfusion_nonfree kwizart"
 
 
-for arch in $ARCHES ; do
+#for arch in $ARCHES ; do
   for repo in $REPOS ; do
     for fver in $FVERSION ; do
 
@@ -19,15 +19,15 @@ for arch in $ARCHES ; do
 
 #sed -i -e "s|@arch@|${arch}|g" fedora-${fver}-${arch}-${repo}.cfg
 #sed -i -e "s|@version@|${fver}|g" fedora-${fver}-${arch}-${repo}.cfg
-for arch2 in armhfp ; do 
-  cp fedora-${fver}-${arch}-${repo}.cfg fedora-${fver}-${arch2}-${repo}.cfg
-  git add fedora-${fver}-${arch2}-${repo}.cfg
-  sed -i -e "s|${arch}|${arch2}|g" fedora-${fver}-${arch2}-${repo}.cfg
-  sed -i -e "s|armhfpv5tel', 'armhfpv6l', 'armhfpv7l|armv7l|g" fedora-${fver}-${arch2}-${repo}.cfg
-  sed -i -e "s|armhfpv5tel|armhl|g" fedora-${fver}-${arch2}-${repo}.cfg
+for arch2 in armhfp arm ppc ppc64 sparc sparc64 s390x ; do 
+  #cp fedora-${fver}-${arch}-${repo}.cfg fedora-${fver}-${arch2}-${repo}.cfg
+  #git add fedora-${fver}-${arch2}-${repo}.cfg
+  #sed -i -e "s|${arch}|${arch2}|g" fedora-${fver}-${arch2}-${repo}.cfg
+  #sed -i -e "s|mirrorlist=http://mirrors.rpmfusion.org|#mirrorlist=http://mirrors.rpmfusion.org|g" fedora-${fver}-${arch2}-${repo}.cfg
+  sed -i -e "s|#baseurl=http://download1.rpmfusion.org/nonfree/fedora/|baseurl=http://download1.rpmfusion.org/nonfree/fedora-secondary/|g" fedora-${fver}-${arch2}-${repo}.cfg
 done
 
 ### /script
     done
   done
-done 
+#done 
