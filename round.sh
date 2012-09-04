bf9fc5a51d512e77f2cffa3c584604492121844e
Nicolas Chauvet <kwizart@gmail.com>
2012-09-04 22:57:35 +0200

    Update to 18.0

--- round.sh
+++ round.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-ARCHES="arm"
-FVERSION="17 rawhide"
+ARCHES="i386 x86_64"
+FVERSION="16 17"
 #FVERSION=rawhide
 REPOS="rpmfusion_free rpmfusion_nonfree kwizart"
 
 
-#for arch in $ARCHES ; do
+for arch in $ARCHES ; do
   for repo in $REPOS ; do
     for fver in $FVERSION ; do
 
@@ -19,16 +19,28 @@ REPOS="rpmfusion_free rpmfusion_nonfree kwizart"
 
 #sed -i -e "s|@arch@|${arch}|g" fedora-${fver}-${arch}-${repo}.cfg
 #sed -i -e "s|@version@|${fver}|g" fedora-${fver}-${arch}-${repo}.cfg
-for arch2 in sparc sparc64 ; do 
-  #cp fedora-${fver}-${arch}-${repo}.cfg fedora-${fver}-${arch2}-${repo}.cfg
+#for arch2 in sparc sparc64 ; do
+  cp /etc/mock/fedora-${fver}-${arch}.cfg fedora-${fver}-${arch}-${repo}.cfg
+  sed -i -e "s|$^\"\"\"||g" fedora-${fver}-${arch}-${repo}.cfg
+  cat rpmfusion-free-stable-template >> fedora-${fver}-${arch}-${repo}.cfg
+  if [ ! "$repo" = rpmfusion_free ] ; then
+    cat rpmfusion-nonfree-stable-template >> fedora-${fver}-${arch}-${repo}.cfg
+  fi
+  cat rpmfusion-buildsys-template >> fedora-${fver}-${arch}-${repo}.cfg
+  if [ "$repo" = kwizart ] ; then
+    cat kwizart-stable-template >> fedora-${fver}-${arch}-${repo}.cfg
+  fi
+  echo "\"\"\"" >> fedora-${fver}-${arch}-${repo}.cfg
   #git add fedora-${fver}-${arch2}-${repo}.cfg
-  #sed -i -e "s|${arch}|${arch2}|g" fedora-${fver}-${arch2}-${repo}.cfg
+  sed -i -e "s|\$basearch|${arch}|g" fedora-${fver}-${arch}-${repo}.cfg
+  sed -i -e "s|\$releasever|${fver}|g" fedora-${fver}-${arch}-${repo}.cfg
   #sed -i -e "s|mirrorlist=http://mirrors.rpmfusion.org|#mirrorlist=http://mirrors.rpmfusion.org|g" fedora-${fver}-${arch2}-${repo}.cfg
-  sed -i -e "s|kojipkgs.fedoraproject.org|sparc.koji.fedoraproject.org|g" fedora-${fver}-${arch2}-${repo}.cfg
+  #sed -i -e "s|kojipkgs.fedoraproject.org|sparc.koji.fedoraproject.org|g" fedora-${fver}-${arch2}-${repo}.cfg
   #sed -i -e "s|#baseurl=http://download1.rpmfusion.org/nonfree/fedora/|baseurl=http://download1.rpmfusion.org/nonfree/fedora-secondary/|g" fedora-${fver}-${arch2}-${repo}.cfg
-done
+#done
 
 ### /script
     done
   done
-#done 
+done
+
