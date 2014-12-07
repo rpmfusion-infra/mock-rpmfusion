75a0ae6b4ac1a6f8c24f344f38b626b4bbfae8fc
Nicolas Chauvet <kwizart@gmail.com>
2014-12-07 15:47:54 +0100

    Fix armhfp is primary since f20

--- round.sh
+++ round.sh
@@ -38,9 +38,10 @@ for arch in $ARCHES ; do
   sed -i -e "s|\$basearch|${arch}|g" fedora-${fver}-${arch}-${repo}.cfg
   sed -i -e "s|\$releasever|${fver}|g" fedora-${fver}-${arch}-${repo}.cfg
   if [  ! $arch == i386 -a ! $arch == x86_64 ] ; then
-    if [ $arch == armhfp -a $fver < 20 ] ; then
-      sed -i -e "s|free/fedora/|free/fedora-secondary/|g" fedora-${fver}-${arch}-${repo}.cfg
+    if [ "$arch" == "armhfp" -a "${ffver}" -gt "19" ] ; then
+      continue
     fi
+    sed -i -e "s|free/fedora/|free/fedora-secondary/|g" fedora-${fver}-${arch}-${repo}.cfg
   fi
   #sed -i -e "s|mirrorlist=http://mirrors.rpmfusion.org|#mirrorlist=http://mirrors.rpmfusion.org|g" fedora-${fver}-${arch2}-${repo}.cfg
   #sed -i -e "s|kojipkgs.fedoraproject.org|sparc.koji.fedoraproject.org|g" fedora-${fver}-${arch2}-${repo}.cfg
