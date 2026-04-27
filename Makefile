206a76efb4a9f81321711778287ff942231fcf52
Sérgio M. Basto <sergio@serjux.com>
2026-04-27 23:31:38 +0100

    F44 GA

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=44.0
+VERSION=44.1
 RELEASE=1
 
 .ONESHELL: # Applies to every targets in the file!
--- round.sh
+++ round.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-ARCHES="i386 x86_64 aarch64 armhfp ppc64le"
-FVERSION="42 43"
-BRANCHED="44"
+ARCHES="i386 x86_64 aarch64 ppc64le"
+FVERSION="42 43 44"
+BRANCHED=""
 RAWHIDE="45"
 REPOS="rpmfusion_free rpmfusion_nonfree"
 etc_mock=../mock/mock-core-configs/etc/mock
